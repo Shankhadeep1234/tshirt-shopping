391331457a6fd389a3ac890c06f028565fdf7ade
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-04-10 13:07:05 +0530

    setting up the initial user schema

---
+++ projbackend/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    maxlength: 32,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+
+  userinfo: {
+    type: String,
+    trim: true,
+  },
+
+  //TODO: come back here
+  password: {
+    type: String,
+    trim: true,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0,
+  },
+  purchases: {
+    type: Array,
+    default: [],
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
